154fc9d3782d2a66eaf9f6c50beebc7d05259c29
AracelyLS <Aracely.Lopez@fundacion-jala.org>
2022-12-06 22:29:41 -0400

    Clase2:avance
    el dinosaurio choca con los obstaculos

---
+++ dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,11 @@
+from dino_runner.components.obstacles.obstacle import Obstacle
+from random import randint
+
+
+class Cactus(Obstacle):
+ Y_POS_LARGE_CACTUS=310
+ def __init__(self,images):
+   super().__init__(images, randint(0,2))
+   self.rect.y=self.Y_POS_LARGE_CACTUS
+
+    
---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,24 @@
+from dino_runner.components.obstacles.cactus   import Cactus
+from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import LARGE_CACTUS
+import pygame
+
+class ObstacleManager:
+    def __init__(self):
+        self.obstacles=[]    
+
+    def update(self, game):
+      if len (self.obstacles)==0:
+         self.obstacles.append(Cactus(LARGE_CACTUS))
+
+      for obstacle in self.obstacles:
+          obstacle.update(game.game_speed,self.obstacles )
+          if game.player.dino_rect.colliderect(obstacle.rect):
+              pygame.time.delay(5000)
+              
+              game.playing=False
+           
+
+    def draw(self,screen):
+      for obstacle in self.obstacles:
+         obstacle.draw(screen)       
